275e4ea209c991ebc6493133dff90efb07f5680d
Alejandro Sandoval <aasandoval@ucdavis.edu>
2018-11-17 01:21:14 -0800

    Update config.js

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,7 +1,7 @@
 export default {
   AUTH0_CLIENT_ID: 'ijhl-sM90wFKgX9qAL8oYoLVs-NXVp6H',
   AUTH0_DOMAIN: 'daywon.auth0.com',
-  REDIRECT_URL: 'http://localhost:3000/callback'
+  REDIRECT_URL: 'https://daywon.herokuapp.com/callback'
 };
 
 
